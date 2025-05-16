38eed6ed92652339c9404a24a9dd0b0c2ded324b
batoqqqa <vas_drug2003@mail.ru>
2025-05-16 20:14:05 +0300

    idk mb it will help to fix deploy problem

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use('/api/users',  requireAuth, requireAdmin, userRoutes);
 
 app.use('/api/orders', require('./routes/orders'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
